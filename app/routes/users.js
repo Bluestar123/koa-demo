dd68a2237f02a5f16647eb4ed4720ee65f867cdc
daywang <daywang@tencent.com>
2021-01-16 20:02:29 +0800

    '使用koa-jwt'

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router')
-const jsonwebtoken = require('jsonwebtoken')
+// const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('koa-jwt')
 const { secret } = require('../config')
 const { findById, create, update, find, del, login, checkOwner } = require('../controllers/user')
 const router = new Router({
@@ -9,21 +10,22 @@ const router = new Router({
 // 先认证（告诉服务器你是谁） 在授权
 
 // 认证中间件
-const auth = async (ctx, next) => {
-  // 解析 token 获取信息
-  const { authorization = '' } = ctx.request.header
-  const token = authorization.replace('Bearer ', '')
-  console.log(token ,456)
-  try {
-    // token 中获取用户信息
-    const user = jsonwebtoken.verify(token, secret)
-    // 存放用户信息
-    ctx.state.user = user
-  } catch (error) {
-    ctx.throw(401, error.message)
-  }
-  await next()
-}
+// const auth = async (ctx, next) => {
+//   // 解析 token 获取信息
+//   const { authorization = '' } = ctx.request.header
+//   const token = authorization.replace('Bearer ', '')
+//   console.log(token ,456)
+//   try {
+//     // token 中获取用户信息
+//     const user = jsonwebtoken.verify(token, secret)
+//     // 存放用户信息
+//     ctx.state.user = user
+//   } catch (error) {
+//     ctx.throw(401, error.message)
+//   }
+//   await next()
+// }
+const auth = jwt({ secret })
 
 router.get('/', auth, find)
 
